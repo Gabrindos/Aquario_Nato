416484ff286dc3e0f1976fd99c9f0ce901749b95
Gabrindos <gabrielsanches1024@outlook.com>
2024-02-29 08:26:18 -0300

    Inicio

---
+++ src/screens/Slide1/index.tsx
@@ -0,0 +1,14 @@
+import { ImageBackground } from "react-native";
+import { styleContainer } from "../../styles/globalstyle."
+
+export function Slide1() {
+   // const slide = require('../../assets/') background image
+    return(
+        <ImageBackground //source={slide} 
+        style={styleContainer.container}>
+
+        </ImageBackground>
+
+        
+    )
+}
---
+++ src/styles/globalstyle..ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const colors = {
+    white : '#FFF',
+    primary: '050a30'
+}
+
+export const styleContainer = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
