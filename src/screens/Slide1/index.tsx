24c086880b84afeb811231d908b5216743e46fd9
Gabrindos <GabrielSanches1024@outlook.com>
2024-03-07 08:28:06 -0300

    Fiz o fundo

--- src/screens/Slide1/index.tsx
+++ src/screens/Slide1/index.tsx
@@ -1,13 +1,17 @@
-import { ImageBackground } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
 import { styleContainer } from "../../styles/globalstyle."
+import React from "react"
 
 export function Slide1() {
-   // const slide = require('../../assets/') background image
-    return(
-        <ImageBackground //source={slide} 
+    const slide = require('../../assets/UltraPeixolas.png')
+        return(
+        <ImageBackground source={slide} 
         style={styleContainer.container}>
-
+        <Text>
+          Entrar 
+        </Text>
         </ImageBackground>
+        
 
         
     )
