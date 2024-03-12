b7cc82c9ff22be8e258f4bf22ee13db9bad3af30
Gabrindos <GabrielSanches1024@outlook.com>
2024-03-12 09:47:50 -0300

    a

--- src/screens/Slide1/index.tsx
+++ src/screens/Slide1/index.tsx
@@ -1,15 +1,17 @@
 import { ImageBackground, Text, View } from "react-native";
-import { styleContainer } from "../../styles/globalstyle."
+import { styleContainer } from "../../styles/globalstyle";
+import { styles } from './style'
 import React from "react"
 
 export function Slide1() {
     const slide = require('../../assets/UltraPeixolas.png')
         return(
-        <ImageBackground source={slide} 
-        style={styleContainer.container}>
-        <Text>
-          Entrar 
-        </Text>
+        <ImageBackground source={slide} style={styleContainer.container}>
+       <View style={styles.view}>
+         <Text style={styles.text}>
+           Entrar 
+         </Text>
+       </View>
         </ImageBackground>
         
 
---
+++ src/screens/Slide1/style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+import { colors } from '../../styles/globalstyle'
+
+export const styles = StyleSheet.create({
+    text:{
+        color: colors.white,
+        fontSize: 25,
+        textAlign: 'center'
+    },
+
+    view: {
+        marginTop: 650,
+
+    }
+
+
+
+
+})
